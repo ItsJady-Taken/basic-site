7ae8ab6aaa48dc797ca49693a729ca328d3e9e90
David <davidkhun2004@gmail.com>
2025-11-19 17:23:06 +0700

    learn node.js basic information site

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const http = require("http");
+const express = require("express");
+const path = require("path");
 
-console.log("Hello, World!");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:8080");
+});
